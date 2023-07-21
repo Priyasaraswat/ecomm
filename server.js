4e561b1ffdc9feba5674d20184059a8c49fd33d2
Priyasaraswat <91541835+Priyasaraswat@users.noreply.github.com>
2023-07-21 19:55:57 +0530

    first commit

--- server.js
+++ server.js
@@ -25,12 +25,14 @@ cloudinary.config({
 })
 
 const __dirname1=path.resolve();
-// console.log(__dirname1)
+console.log(__dirname1);
+console.log(path.resolve(__dirname1,"frontend", "build", "index.html"))
 
  if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
  app.get("*", (req, res) =>
+   
     res.sendFile(path.resolve(__dirname1,"frontend", "build", "index.html"))
   );
 } else {
